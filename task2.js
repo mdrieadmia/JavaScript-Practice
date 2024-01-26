963e2e033b82a1c962d8b21376a33cccac471cb5
riead <rieadyt@gmail.com>
2024-01-26 23:59:02 +0600

    Task 2 done

---
+++ task2.js
@@ -0,0 +1,8 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35;
+var bangla = 99.53;
+var averageMark = (mathematics + biology + chemistry + physics + bangla) / 5;
+console.log(averageMark.toFixed(2));
+
