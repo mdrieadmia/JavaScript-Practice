4acfc587ad30401d0f4869667ee085a3a46f6d0f
riead <rieadyt@gmail.com>
2024-01-27 00:24:27 +0600

    Bmi calculator task done

---
+++ BMI_calculator.js
@@ -0,0 +1,16 @@
+var weight = 62;
+var height = 1.67;
+var bmi = weight / height**2;
+console.log(bmi.toFixed(2));
+if(bmi<18.5){
+    console.log("You'r Underweight");
+}
+else if(bmi >= 18.5 && bmi <=24.9){
+    console.log("You'r Normal");
+}
+else if(bmi >=25 && bmi <= 29.9){
+    console.log("You'r Overweight");
+}
+else{
+    console.log("You'r Obese");
+}
