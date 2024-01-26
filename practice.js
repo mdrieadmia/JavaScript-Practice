040e33c3b1cb003b7e1ce63f55cfd54d958649ce
riead <rieadyt@gmail.com>
2024-01-26 18:28:04 +0600

    Task 1 Done

---
+++ practice.js
@@ -0,0 +1,6 @@
+var givenTaka = 1000;
+var orangePrice = 500;
+var applePrice = 200;
+var totalPrice = orangePrice + applePrice;
+var returnPrice = givenTaka - totalPrice;
+console.log(returnPrice);
