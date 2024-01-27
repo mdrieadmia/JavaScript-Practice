e1fa32d4df12ef3bb2e4a84a9411807050ef330a
riead <rieadyt@gmail.com>
2024-01-27 13:02:51 +0600

    Ternary number task done

---
+++ Ternary_number.js
@@ -0,0 +1,15 @@
+var num1 = 50;
+var num2 = 40;
+var result;
+// Simple if-else program
+if (num1 > num2) {
+  result = num1 * 2;
+  console.log(result);
+} 
+else {
+  result = num1 + num2;
+  console.log(result);
+}
+// Ternary operator program
+result = num1 > num2 ? num1*2 : num1+num2;
+console.log(result);
